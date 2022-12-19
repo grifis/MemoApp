feccc8c384892a27a48260d172d5e69381e3f627
grifis <fujeba2abm@i.softbank.jp>
2022-12-20 02:40:42 +0900

    Auto Login and logout button

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import LogOutButton from '../components/LogOutButton';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
   return (
     <View style={styles.container}>
       <MemoList />
